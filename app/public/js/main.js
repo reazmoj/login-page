78f82514e185131b6cd1204e7037b4309cda0998
reazmoj <reazmoj.h@gmail.com>
2022-08-16 20:36:04 +0430

    not worked...

---
+++ app/public/js/main.js
@@ -0,0 +1,35 @@
+// const username = document.getElementById("username");
+// const password = document.getElementById("password");
+// const loginButton = document.getElementById("loginButton");
+
+// document.addEventListener("keyup", (event) => {
+//   isValidUsername = username.checkValidity();
+//   isValidPassword = password.checkValidity();
+
+//   // console.log(username.value, password.value);
+
+//   if (isValidUsername & isValidPassword) {
+//     document.getElementById("loginButton").disabled = false;
+//   } else {
+//     document.getElementById("loginButton").disabled = true;
+//   }
+// });
+
+// const togglePassword = document.getElementById("togglePassword");
+
+// togglePassword.addEventListener("click", function () {
+//   // toggle the type attribute
+//   const type =
+//     password.getAttribute("type") === "password" ? "text" : "password";
+//   password.setAttribute("type", type);
+
+//   // toggle the span textContent
+// });
+
+// password.addEventListener("keyup", (event) => {
+//   if (password.value.length > 0) {
+//     password.style.display = "block";
+//   } else {
+//     password.style.display = "none";
+//   }
+// });
