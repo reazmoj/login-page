37a4cd522a0345aac20bbc096ff5bfe9497ce963
reazmoj <reazmoj.h@gmail.com>
2022-08-16 20:33:43 +0430

    add session part into project

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+
+const app = express();
 
 /*The server can use gzip compression to reduce file sizes
 before sending them to a web browser. 
 This will reduce latency and lag.*/
 const compression = require("compression");
+
 const bodyParser = require("body-parser");
 
 const errorController = require("./app/components/error/error");
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}.`);
@@ -24,9 +26,15 @@ async function inint() {
   app.use(express.static(path.join(__dirname, "app/public")));
   app.set("view engine", "ejs");
   app.set("views", "./app/views");
-
-  // simple error page rout ...!!!
-  
+  app.set("trust proxy", 1); // trust first proxy
+  app.use(
+    session({
+      secret: "keyboard cat",
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: true },
+    })
+  );
 
   try {
     const Components = require("./app/components/index");
@@ -40,3 +48,22 @@ async function inint() {
 }
 
 inint();
+
+// This section is commented these are just for optioning:
+
+/*********************************************************************************
+For using secure cookies in production, but allowing for testing in development, *
+the following is an example of enabling this setup based on NODE_ENV in express: *
+                                                                                 *
+const sess = {                                                                   *
+  secret: 'keyboard cat',                                                        *
+  cookie: {}                                                                     *
+}                                                                                *  
+                                                                                 *
+if (app.get('env') === 'production') {                                           *
+  app.set('trust proxy', 1) // trust first proxy                                 *
+  sess.cookie.secure = true // serve secure cookies                              *
+}                                                                                *
+                                                                                 *
+app.use(session(sess))                                                           *
+**********************************************************************************/
