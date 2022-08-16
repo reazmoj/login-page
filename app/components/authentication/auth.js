ee9bd801d3fc5e9e484d2fcf21dcb817f4862cf7
reazmoj <reazmoj.h@gmail.com>
2022-08-16 20:35:15 +0430

    update postlogin api function

--- app/components/authentication/auth.js
+++ app/components/authentication/auth.js
@@ -1,12 +1,32 @@
 const User = require("../user-component/user");
 const authDal = require("./authDAL");
 
+const fotterItems = {
+  item1: [
+    "Meta",
+    "About",
+    "Blog",
+    "Jobs",
+    "Help",
+    "API",
+    "Privacy",
+    "Terms",
+    "Top Accounts",
+    "Hashtags",
+    "Locations",
+    "Instagram Lite",
+    "Contact Uploading & Non-Users",
+  ],
+  item2: ["Dance", "Food & Drink", "Home & Garden", "Music", "Visual Arts"],
+};
+
 class Auth {
   async getLogin(req, res, next) {
     try {
       res.render("auth/login", {
         path: "/login",
-        pageTitle: "Login",
+        pageTitle: "Instagram Login",
+        data: fotterItems,
       });
     } catch (error) {
       console.log(error);
@@ -17,22 +37,31 @@ class Auth {
     try {
       const userInfo = new User(req.body.Username, req.body.Password);
       const dbResult = await authDal.getLogin(userInfo);
-      return res.json(dbResult);
+      if (dbResult["returnValue"] == 1) {
+        return res.status(200).send({
+          Success: true,
+          Message: "successfully member's info added to database.",
+        });
+      } else {
+        return res.send({
+          Success: false,
+          Message: "Input data is invalid or exist!",
+        });
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
-  async getRegister (req, res, next) {};
+  async getRegister(req, res, next) {}
 
-  async getResetPass (req, res, next) {};
+  async getResetPass(req, res, next) {}
 
-  async getForgotPass (req, res, next) {};
+  async getForgotPass(req, res, next) {}
 
-  async postRegister (req, res, next) {};
+  async postRegister(req, res, next) {}
 
-  async postLogout (req, res, next) {};
+  async postLogout(req, res, next) {}
 }
-
 
 module.exports = new Auth();
